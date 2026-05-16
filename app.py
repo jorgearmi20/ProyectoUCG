c314988bd7da4255dcd74d9061a4614b239ec37e
jorgearmi20 <30957334+jorgearmi20@users.noreply.github.com>
2026-05-16 09:52:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
-
+import pandas as pd
 st.title("Proyecto Final UCG - Jorge Armijos")
 st.header("Propuesta")
 st.badge("Sistema")
 st.sidebar.title("Parámetros")
 st.sidebar.image("python.png")
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
